689bb075b3b05d0375f85ee1c6d9dd6678149f89
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-04-12 18:04:23 +0200

    Add CreateSettlement method and Request and Response DTOs

---
+++ Krecha.Lib/Services/Responses/CreateSettlementResponse.cs
@@ -0,0 +1,8 @@
+﻿using Krecha.Lib.Data.Models;
+
+namespace Krecha.Lib.Services.Responses;
+public class CreateSettlementResponse
+{
+    public bool Success { get; set; }
+    public int? CreatedSettlementId { get; set; } = null;
+}
--- Krecha.Lib/Services/SettlementsService.cs
+++ Krecha.Lib/Services/SettlementsService.cs
@@ -1,4 +1,7 @@
 ﻿using Krecha.Lib.Data;
+using Krecha.Lib.Data.Models;
+using Krecha.Lib.Services.Requests;
+using Krecha.Lib.Services.Responses;
 
 namespace Krecha.Lib.Services;
 public class SettlementsService
@@ -8,5 +11,32 @@ public class SettlementsService
     public SettlementsService(SettlementsDbContext dbContext)
     {
         _dbContext = dbContext;
+    }
+
+    public async Task<CreateSettlementResponse> CreateSettlement(CreateSettlementRequest request)
+    {
+        CreateSettlementResponse response = new();
+
+        Currency? currency = _dbContext.Currencies
+            .FirstOrDefault(c => c.Id == request.CurrencyId);
+
+        if (currency is null)
+        {
+            response.Success = false;
+            return response;
+        }
+
+        Settlement toCreate = new()
+        {
+            Name = request.Name,
+            Description = request.Description,
+            Currency = currency,
+        };
+
+        await _dbContext.Settlements.AddAsync(toCreate);
+        await _dbContext.SaveChangesAsync();
+
+        response.CreatedSettlementId = toCreate.Id;
+        return response;
     }
 }
