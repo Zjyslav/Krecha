c0565d44ef70e4c1b814958824173b7c4502644e
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-04-12 17:52:45 +0200

    Add SettlementsService

---
+++ Krecha.Lib/Services/SettlementsService.cs
@@ -0,0 +1,12 @@
+﻿using Krecha.Lib.Data;
+
+namespace Krecha.Lib.Services;
+public class SettlementsService
+{
+    private readonly SettlementsDbContext _dbContext;
+
+    public SettlementsService(SettlementsDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+}
