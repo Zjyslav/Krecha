c5b96112e6a57472d5566df63fe0bdddb2a3e2d0
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-04-17 20:46:05 +0200

    Add generic repository class and replace DbContext in SettlementService with it

---
+++ Krecha.Lib/Data/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Krecha.Lib.Data;
+public class Repository<T>
+    where T : class
+{
+    private readonly SettlementsDbContext _dbContext;
+
+    public Repository(SettlementsDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IQueryable<T> GetAll() => _dbContext.Set<T>().AsQueryable();
+    public async Task<T?> GetById<U>(U id) => await _dbContext.Set<T>().FindAsync(id);
+
+    public async Task<T?> Create(T entity)
+    {
+        _dbContext.Set<T>().Add(entity);
+        await _dbContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<T?> Update<U>(U id, Action<T> update)
+    {
+        var toUpdate = await GetById(id);
+        if (toUpdate is null)
+            return null;
+
+        update.Invoke(toUpdate);
+        await _dbContext.SaveChangesAsync();
+
+        return toUpdate;
+    }
+
+    public async Task<T?> Delete<U>(U id)
+    {
+        var toDelete = await GetById(id);
+        if (toDelete is null)
+            return null;
+
+        _dbContext.Set<T>().Remove(toDelete);
+        await _dbContext.SaveChangesAsync();
+
+        return toDelete;
+    }
+}
