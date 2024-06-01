1a645569b96e0dd7925a71533cfede9ad98b7533
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-06-01 23:19:49 +0200

    Add SettlementsServiceTetstBase

---
+++ Tests/Krecha.Lib.Tests/Services/Settlements/SettlementsServiceTetstBase.cs
@@ -0,0 +1,24 @@
+﻿using Krecha.Lib.Data;
+using Krecha.Lib.Data.Models;
+using Krecha.Lib.Interfaces.Data;
+using Krecha.Lib.Services;
+using Krecha.Lib.Tests.Helpers;
+using Moq;
+
+namespace Krecha.Lib.Tests.Services.Settlements;
+public abstract class SettlementsServiceTetstBase
+{
+    protected SettlementsService SettlementsService { get; }
+    protected Mock<IRepository<Currency>> MockCurrencyRepository { get; } = new();
+    protected Mock<IRepository<Settlement>> MockSettlementRepository { get; } = new();
+    protected Mock<IRepository<SettlementEntry>> MockSettlementEntryRepository { get; } = new();
+    protected SettlementsDbContext DbContext { get; }
+    protected SettlementsServiceTetstBase()
+    {
+        SettlementsService = new(MockCurrencyRepository.Object,
+                                 MockSettlementRepository.Object,
+                                 MockSettlementEntryRepository.Object);
+
+        DbContext = EFHelpers.SetupInMemoryDbContext();
+    }
+}
