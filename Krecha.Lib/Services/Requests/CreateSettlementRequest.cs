5290eef5803b9bc6b4e859216b3d2ca230846afd
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-06-09 13:52:55 +0200

    Make request DTOs immutable and add argument validation

--- Krecha.Lib/Services/Requests/CreateSettlementRequest.cs
+++ Krecha.Lib/Services/Requests/CreateSettlementRequest.cs
@@ -3,12 +3,27 @@ public class CreateSettlementRequest
 {
     public CreateSettlementRequest(string name, string description, int currencyId)
     {
+        ValidateConstructorArguments(name, description);
+
         Name = name;
         Description = description;
         CurrencyId = currencyId;
     }
 
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public int CurrencyId { get; set; }
+    public string Name { get; }
+    public string Description { get; }
+    public int CurrencyId { get; }
+
+    private static void ValidateConstructorArguments(string name, string description)
+    {
+        if (name is null)
+        {
+            throw new ArgumentNullException(nameof(name));
+        }
+
+        if (description is null)
+        {
+            throw new ArgumentNullException(nameof(description));
+        }
+    }
 }
