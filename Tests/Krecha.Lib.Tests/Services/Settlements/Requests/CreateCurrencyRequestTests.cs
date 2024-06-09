b9c0c69e79d4e6a7f147d4ea4eff38688d902231
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-06-09 13:53:16 +0200

    Add tests for argument validation of create request DTOs

---
+++ Tests/Krecha.Lib.Tests/Services/Settlements/Requests/CreateCurrencyRequestTests.cs
@@ -0,0 +1,56 @@
+﻿using Krecha.Lib.Data.Models;
+using Krecha.Lib.Services.Requests;
+using System.ComponentModel;
+
+namespace Krecha.Lib.Tests.Services.Settlements.Requests;
+public class CreateCurrencyRequestTests
+{
+    [Fact]
+    public void CreateCurrencyRequestConstructor_WhenNameIsNull_ShouldThrowCorrectException()
+    {
+        // Arrange
+        string name = null!;
+        string symbol = "Test symbol";
+        CurrencySymbolPosition symbolPosition = CurrencySymbolPosition.After;
+
+        // Act
+        CreateCurrencyRequest act() => new CreateCurrencyRequest(name, symbol, symbolPosition);
+
+        // Assert
+        var exception = Assert.Throws<ArgumentNullException>(act);
+        Assert.Equal(nameof(name), exception.ParamName);
+    }
+
+    [Fact]
+    public void CreateCurrencyRequestConstructor_WhenSymbolIsNull_ShouldThrowCorrectException()
+    {
+        // Arrange
+        string name = "Test name";
+        string symbol = null!;
+        CurrencySymbolPosition symbolPosition = CurrencySymbolPosition.After;
+
+        // Act
+        CreateCurrencyRequest act() => new CreateCurrencyRequest(name, symbol, symbolPosition);
+
+        // Assert
+        var exception = Assert.Throws<ArgumentNullException>(act);
+        Assert.Equal(nameof(symbol), exception.ParamName);
+    }
+
+    [Fact]
+    public void CreateCurrencyRequestConstructor_WhenSymbolPositionIsInvalid_ShouldThrowCorrectException()
+    {
+        // Arrange
+        string name = "Test name";
+        string symbol = "Test symbol";
+        int incorrectEnumValue = -1;
+        CurrencySymbolPosition symbolPosition = (CurrencySymbolPosition)incorrectEnumValue;
+
+        // Act
+        CreateCurrencyRequest act() => new CreateCurrencyRequest(name, symbol, symbolPosition);
+
+        // Assert
+        var exception = Assert.Throws<InvalidEnumArgumentException>(act);
+        Assert.Equal(nameof(symbolPosition), exception.ParamName);
+    }
+}
---
+++ Tests/Krecha.Lib.Tests/Services/Settlements/Requests/CreateSettlementRequestTests.cs
@@ -0,0 +1,37 @@
+﻿using Krecha.Lib.Services.Requests;
+
+namespace Krecha.Lib.Tests.Services.Settlements.Requests;
+public class CreateSettlementRequestTests
+{
+    [Fact]
+    public void CreateSettlementRequestConstructor_WhenNameIsNull_ShouldThrowCorrectException()
+    {
+        // Arrange
+        string name = null!;
+        string description = "Test description";
+        int currencyId = 19;
+
+        // Act
+        CreateSettlementRequest act() => new CreateSettlementRequest(name, description, currencyId);
+
+        // Assert
+        var exception = Assert.Throws<ArgumentNullException>(act);
+        Assert.Equal(nameof(name), exception.ParamName);
+    }
+
+    [Fact]
+    public void CreateSettlementRequestConstructor_WhenDescriptionIsNull_ShouldThrowCorrectException()
+    {
+        // Arrange
+        string name = "Test name";
+        string description = null!;
+        int currencyId = 19;
+
+        // Act
+        CreateSettlementRequest act() => new CreateSettlementRequest(name, description, currencyId);
+
+        // Assert
+        var exception = Assert.Throws<ArgumentNullException>(act);
+        Assert.Equal(nameof(description), exception.ParamName);
+    }
+}
